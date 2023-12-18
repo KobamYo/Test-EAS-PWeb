29a345ce207dfe7e98d8e8984d9fb6c5a16a45b3
Wahyu Tri Saputro <91375670+KobamYo@users.noreply.github.com>
2023-12-19 00:08:03 +0700

    Update ShowChannelView.vue

--- eas-p-web/src/views/ShowChannelView.vue
+++ eas-p-web/src/views/ShowChannelView.vue
@@ -12,9 +12,75 @@
             alert(e.message);
         }
     }
+    getData();
+
+    const nameChannel = ref('');
+    const createChannel = async () => {
+        try {
+            const res = await axios.post('http://localhost:3000/api/channels', {
+                channelName: nameChannel.value
+            }, {
+                withCredentials: true
+            })
+            console.log(res);
+            alert('Create Success')
+        } catch (e) {
+            alert(e.message);
+        }
+    }
+
+    const selectedId = ref('')
+    const nameUpdateChannel = ref('')
+    const updateChannel = async () => {
+        try {
+            const res = await axios.patch('http://localhost:3000/api/channels/${selected.value}', {
+                channelName: nameUpdateChannel.value
+            }, {
+                withCredentials: true
+            })
+            console.log(res);
+            alert('Update Success')
+        } catch (e) {
+            console.log(e)
+            alert(e.message);
+        }
+    }
+
+    const deleteChannel = async () => {
+        try {
+            const res = await axios.delete('http://localhost:3000/api/channels/${selected.value}', {
+                withCredentials: true
+            })
+            console.log(res);
+            alert('Delete Success')
+        } catch (e) {
+            console.log(e)
+            alert(e.message);
+        }
+    }
 </script>
  
  <template>
-    Channels
-
+    Channels :
+    <div v-for="item in channels.docs">
+        <div @click="() => selectedId=item.id">{{ item.channelName }}</div>
+    </div>
+    <br/>
+    Create Channels
+    <form @submit="createChannel">
+        <input type="text" v-model="nameChannel">
+        <button type="submit">Create</button>
+    </form>
+    <br/>
+    Seleted id Channel : {{ selected }}
+    <br/>
+    updateChannel
+    <form @submit="updateChannel">
+        <input type="text" v-model="nameUpdateChannel">
+        <button type="submit">Update</button>
+    </form>
+    <br/>
+    <form @submit="deleteChannel">
+        <button type="submit">Delete</button>
+    </form>
  </template>
