86e010fd10e69dcc237cb085ca99d8a29b995ff7
Wahyu Tri Saputro <91375670+KobamYo@users.noreply.github.com>
2023-12-19 09:02:30 +0700

    aedwda

--- eas-p-web/my-vue-app/src/router/index.js
+++ eas-p-web/my-vue-app/src/router/index.js
@@ -1,19 +1,29 @@
-// // {
-// //     path: '/about',
-// //     name: about,
-// //     component: () => import('../views/AboutView.vue')
-// // },
-// {
-//     path: '/login',
-//     component: () => import('../views/LoginView.vue')
-// },
-// {
-//     path: '/channels',
-//     component: () => import('../views/ShowChannelView.vue')
-// },
-// {
-//     path: '/register',
-//     component: () => import('../views/RegisterView.vue')
-// }
+import { createRouter, createWebHistory } from 'my-vue-app'
+import HomeView from '../views/HomeView.vue'
 
-// export default router
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: about,
+        component: () => import('../views/AboutView.vue')
+    },
+    {
+        path: '/login',
+        component: () => import('../views/LoginView.vue')
+    },
+    {
+        path: '/channels',
+        component: () => import('../views/ShowChannelView.vue')
+    },
+    {
+        path: '/register',
+        component: () => import('../views/RegisterView.vue')
+    }
+]
+
+export default router
